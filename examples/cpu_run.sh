09143b34517d218a7f1636d0d143bf5106a442cc
XiaobingZhang <1489479870@qq.com>
2019-01-23 15:22:41 +0800

    change the run squad script to do only tranining

--- examples/cpu_run.sh
+++ examples/cpu_run.sh
@@ -6,13 +6,11 @@ export SQUAD_DIR=SQUAD
 python run_squad.py \
     --bert_model bert-base-uncased \
     --do_train \
-    --do_predict \
     --do_lower_case \
     --train_file $SQUAD_DIR/train-v1.1.json \
-    --predict_file $SQUAD_DIR/dev-v1.1.json \
     --train_batch_size 12 \
     --learning_rate 3e-5 \
     --num_train_epochs 1.0 \
     --max_seq_length 384 \
     --doc_stride 128 \
-    --output_dir /tmp/debug_squad/
+    --output_dir output/squad/
